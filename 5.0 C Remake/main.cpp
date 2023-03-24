cac89684baf2b775f0a731d20eeed8ede6b8dd6d
JRH3221 <ts.dog3221@gmail.com>
2023-03-24 22:44:32 +1100

    Added Front wheel calculations

--- 5.0 C Remake/main.cpp
+++ 5.0 C Remake/main.cpp
@@ -6,10 +6,12 @@
 #define BACK_WHEEL 180
 #define FRONT_RIGHT_WHEEL 300
 
+//The reason for the different speeds, is to allow the speed to be changed once for testing, instead of changing every instance of speed
 #define MAX_SPEED 100 // this is the highest speed that calc variables will accept, the limit that MAX_SPEED can be set to is 1200, which means you are just setting raw speed
 #define RUN_SPEED 50 // (MAX_SPEED) will most likely be used when out of testing
 #define SPEED_CONST (1200/MAX_SPEED)
 #define PI 3.1416
+#define RAD_CONST (PI/180)
 
 // I don't know if header files are accepted, so i will do all class definitions in the main implemenation file for now
 
@@ -22,19 +24,28 @@ private:
 };
 
 void Program::Run(int8_t calcSpeed, int16_t heading, int8_t calcTurn) {
-	int16_t rawSpeed = calcSpeed * SPEED_CONST;
+	int16_t rawSpeed = calcSpeed * SPEED_CONST; //In final production this can be removed, as all calls to Run() should pass MAX_SPEED, with MAX_SPEED set to 1200
 
 
 	int16_t θ1 = BACK_WHEEL - heading;
-	float M1 = std::round(rawSpeed * std::sin(θ1 * (PI/180))); //using round so there is limited drift due to pi being an approximation
+	float M1 = std::round(rawSpeed * std::sin(θ1 * RAD_CONST)); //using round so there is limited drift due to pi being an approximation
+	M1 += calcTurn * SPEED_CONST; //Add rotation, input is 0 - 100
 
-	std::cout << "Back wheel speed: " << M1;
+	int16_t θ2 = FRONT_RIGHT_WHEEL - heading;
+	float M2 = std::round(rawSpeed * std::sin(θ2 * RAD_CONST));
+	M2 += calcTurn * SPEED_CONST;
+
+	int16_t θ3 = FRONT_LEFT_WHEEL - heading;
+	float M3 = std::round(rawSpeed * std::sin(θ3 * RAD_CONST));
+	M3 += calcTurn * SPEED_CONST;
+
+	std::cout << "Back wheel speed: " << M1 << '\n' << "Right Wheel Speed: " << M2 << std::endl;
 }
 
 int main() { //Entry Point
 	Program* program = new Program;
 
-	program->Run(100, 0, 0);
+	program->Run(RUN_SPEED, 120, 0);
 
 	return 0;
 }
