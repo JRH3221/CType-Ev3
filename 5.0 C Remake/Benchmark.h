bcf0b0fb02a9d21faab17e6c404c79ef94748fcc
JRH3221 <ts.dog3221@gmail.com>
2023-03-24 23:17:10 +1100

    Added Benchmarking

---
+++ 5.0 C Remake/Benchmark.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+class Benchmark {
+
+public:
+
+	Benchmark();
+
+private:
+	static void SystemOut();
+
+};
--- 5.0 C Remake/main.cpp
+++ 5.0 C Remake/main.cpp
@@ -1,5 +1,8 @@
 ﻿#include <iostream>
 #include <cmath>
+#include <thread>
+
+#include "Benchmark.h"
 
 // Used for run speed calculations of each wheel
 #define FRONT_LEFT_WHEEL 60
@@ -18,32 +21,47 @@
 class Program {
 public:
 	void Run(int8_t calcSpeed, int16_t heading, int8_t calcTurn); // calcSpeed and calcTurn are 0-100, they will be processed into raw speed
-
+	bool running = false;
+	static void SystemOut();
+	void Benchmark();
 private:
+	int16_t θ1;
+	float M1;
 
+	int16_t θ2;
+	float M2;
+
+	int16_t θ3;
+	float M3;
 };
 
 void Program::Run(int8_t calcSpeed, int16_t heading, int8_t calcTurn) {
 	int16_t rawSpeed = calcSpeed * SPEED_CONST; //In final production this can be removed, as all calls to Run() should pass MAX_SPEED, with MAX_SPEED set to 1200
 
 
-	int16_t θ1 = BACK_WHEEL - heading;
-	float M1 = std::round(rawSpeed * std::sin(θ1 * RAD_CONST)); //using round so there is limited drift due to pi being an approximation
+	θ1 = BACK_WHEEL - heading;
+	M1 = std::round(rawSpeed * std::sin(θ1 * RAD_CONST)); //using round so there is limited drift due to pi being an approximation
 	M1 += calcTurn * SPEED_CONST; //Add rotation, input is 0 - 100
 
-	int16_t θ2 = FRONT_RIGHT_WHEEL - heading;
-	float M2 = std::round(rawSpeed * std::sin(θ2 * RAD_CONST));
+	θ2 = FRONT_RIGHT_WHEEL - heading;
+	M2 = std::round(rawSpeed * std::sin(θ2 * RAD_CONST));
 	M2 += calcTurn * SPEED_CONST;
 
-	int16_t θ3 = FRONT_LEFT_WHEEL - heading;
-	float M3 = std::round(rawSpeed * std::sin(θ3 * RAD_CONST));
+	θ3 = FRONT_LEFT_WHEEL - heading;
+	M3 = std::round(rawSpeed * std::sin(θ3 * RAD_CONST));
 	M3 += calcTurn * SPEED_CONST;
+}
 
-	std::cout << "Back wheel speed: " << M1 << '\n' << "Right Wheel Speed: " << M2 << std::endl;
+void Program::Benchmark() {
+	std::thread benchmarking(SystemOut);
 }
 
 int main() { //Entry Point
 	Program* program = new Program;
+	//Benchmark* benchmark = new Benchmark; // Constructor called <- remove this code in release
+
+	program->Benchmark();
+
 
 	program->Run(RUN_SPEED, 120, 0);
 
